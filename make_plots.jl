7908f1c49764a85978b1ce0ff06def82a0c70fc4
Michael Bennett <michael.d.a.bennett@outlook.com>
2023-05-08 16:36:10 -0400

    finalise plotting script

--- make_plots.jl
+++ make_plots.jl
@@ -1,15 +1,22 @@
-using PlotlyJS
+using PlotlyJS, PlotlyBase
+
+const m = 0
 
 function make_3d_plot(
-        h_grid, a_grid, outcome_mats, names, title, zax_title, out;
+        h_grid, a_grid, outcome_mats, names, title, zax_title, camera, out;
         include_zero=false
     )
     layout = Layout(
-        title=title,
+        showlegend=true,
+        autosize=false,
+        width=700, height=600, margin=attr(l=0, r=0, b=0, t=0),
         scene=attr(
             xaxis_title="Human Capital",
             yaxis_title="Wealth",
-            zaxis_title=zax_title
+            zaxis_title=zax_title,
+            legend_title="Employment",
+            aspectmode="cube",
+            camera=camera
         )
     )
 
@@ -18,7 +25,7 @@ function make_3d_plot(
     outcome_plots = [
             surface(
                 x=h_grid, y=a_grid, z=outcome_mat, name=name,
-                colorscale=cscale, showscale=false
+                colorscale=cscale, showscale=false, showlegend=true
             ) for (outcome_mat, name, cscale)
             ∈ zip(outcome_mats, names, colourscale)
         ]
@@ -38,9 +45,14 @@ function make_3d_plot(
     )
 
     filename = joinpath(@__DIR__, "plots", "$out.html")
+    svgfile = joinpath(@__DIR__, "plots", "$out.pdf")
     touch(filename)
     open(filename, "w") do io
         PlotlyBase.to_html(io, p.plot)
+    end
+    touch(svgfile)
+    open(svgfile, "w") do io
+        savefig(io, p.plot, format="pdf")
     end
     return p
 end
