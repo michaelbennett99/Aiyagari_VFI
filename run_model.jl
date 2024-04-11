168df28e54e6022bedae1988db703ea9f9023a01
Michael Bennett <michael.d.a.bennett@outlook.com>
2024-04-11 19:26:43 -0400

    remove PFI and make consts const

--- run_model.jl
+++ run_model.jl
@@ -4,36 +4,36 @@ using Serialization
 include("stochastic_employment.jl")
 
 # Set parameters
-β = 0.9f0
+const β = 0.9f0
 
-δ = 0.1f0
-α  = 1f0
-η = 0f0
-A = 1.5f0
+const δ = 0.1f0
+const α  = 1f0
+const η = 0f0
+const A = 1.5f0
 
-γ = 1f0
-θ = 1.5f0
-ε = 0.5f0
+const γ = 1f0
+const θ = 1.5f0
+const ε = 0.5f0
 
-z = 1f0
-r = 0.1f0
+const z = 1f0
+const r = 0.1f0
 
-λ_f = 0.95f0
-λ_l = 0.05f0
+const λ_f = 0.95f0
+const λ_l = 0.05f0
 
 # Set up grid lengths
-h_N = 200
-a_N = 200
-e_N = 2
+const h_N = 200
+const a_N = 200
+const e_N = 2
 
 # Define parameters
-a_lbar = -z / r * 0.99f0
-h_bar = (A /  δ)^(1 / (1-η))
+const a_lbar = -z / r * 0.99f0
+const h_bar = (A /  δ)^(1 / (1-η))
 
 # Set up grids
-h_grid = collect(Float32, range(z, h_bar, h_N))
-a_grid = collect(Float32, range(a_lbar, -a_lbar, a_N))
-e_grid, trans_mat = calculate_emp_process(λ_f, λ_l)
+const h_grid = collect(Float32, range(z, h_bar, h_N))
+const a_grid = collect(Float32, range(a_lbar, -a_lbar, a_N))
+const e_grid, trans_mat = calculate_emp_process(λ_f, λ_l)
 
 if !isinteractive()
     results = do_VFI(
@@ -43,14 +43,5 @@ if !isinteractive()
 
     open(joinpath(@__DIR__, "results", "VFI_results.jls"), "w") do io
         serialize(io, results)
-    end
-
-    results2 = do_PFI(
-        flow_value, h_grid, a_grid, e_grid, trans_mat, β,
-        δ=δ, α=α, η=η, A=A, γ=γ, θ=θ, ε=ε, z=z, r=r, tol=1f-6
-    )
-
-    open(joinpath(@__DIR__, "results", "PFI_results.jls"), "w") do io
-        serialize(io, results2)
     end
 end
